a146646f57f4fd51af83b6cbe913b633b033f93e
Vladimir Kovalenko <vovkazon@mail.ru>
2020-03-20 22:33:07 +0300

    Tip counter project. Connected outlets and made view

---
+++ TIP ME/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TIP ME
+//
+//  Created by Владимир Коваленко on 20.03.2020.
+//  Copyright © 2020 Vladimir Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tenPntButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var twentyPntButton: UIButton!
+    @IBOutlet weak var ZeroPntButton: UIButton!
+   
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ TIP ME/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  TIP ME
+//
+//  Created by Владимир Коваленко on 20.03.2020.
+//  Copyright © 2020 Vladimir Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var splitBtwnPeopleLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func recalculateButtonPressed(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
